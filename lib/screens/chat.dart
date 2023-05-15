e8ae91fd2a4f2750a0333d2f56be8fdc5e2d7cd7
Rajvardhan <patilrajvardhan27@gmail.com>
2023-05-15 10:03:54 +0530

    working with notifications

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,12 +1,34 @@
 import 'package:chatapp/widgets/chat_message.dart';
 import 'package:chatapp/widgets/new_message.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
-class chatScreen extends StatelessWidget {
+import 'package:chatapp/widgets/chat_message.dart';
+import 'package:chatapp/widgets/new_message.dart';
+
+class chatScreen extends StatefulWidget {
   const chatScreen({super.key});
 
   @override
+  State<chatScreen> createState() => _chatScreenState();
+}
+
+class _chatScreenState extends State<chatScreen> {
+  void setupPushNotifications() async {
+    final fcm = FirebaseMessaging.instance;
+
+    await fcm.requestPermission();
+    fcm.subscribeToTopic('chat');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    setupPushNotifications();
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
